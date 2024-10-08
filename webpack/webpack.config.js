cc6d18c8cd7ae0343778f62e9cdd6bcad2b1570e
sayan39039 <sayan39039@gmail.com>
2024-10-08 21:39:48 +0530

    Cleaned master branch

--- webpack/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
-
-module.exports = (envVar) => {
-  const { env } = envVar;
-  const variableConfig = require(`./webpack.${env}.js`);
-  const congig = merge(commonConfig, variableConfig);
-  return congig;
-};
